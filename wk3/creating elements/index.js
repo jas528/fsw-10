2c031010e9099fde755e8c9516f6c8924c3feef3
jas528 <jasmine.pipkins@bryanuniversity.edu>
2021-04-26 14:50:07 -0500

    wk 3

---
+++ wk3/creating elements/index.js
@@ -0,0 +1,23 @@
+var heading = document.createElement('h1')
+document.body.append(heading) 
+heading.textContent ="Welcome to my JS Site"
+
+var paragraph = document.createElement('p')
+document.body.append(paragraph) 
+paragraph.textContent ="All of this was created wit js"
+
+var ol = document.createElement('ol')
+document.body.append(ol) 
+ol.textContent ="Welcome to my JS Site"
+
+var li = document.createElement('li')
+document.body.append(li) 
+li.textContent ="eggs,milk butter"
+
+var li = document.createElement('li')
+document.body.append(li) 
+li.textContent ="water"
+
+var li = document.createElement('li')
+document.body.append(li) 
+li.textContent ="soap"
