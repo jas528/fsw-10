e3853f2ed6524b16c4f5c543d71df3c74687df1e
jas528 <jasmine.pipkins@bryanuniversity.edu>
2021-05-13 13:04:35 -0500

    2

---
+++ wk5/modifyelements/index.js
@@ -0,0 +1,12 @@
+const inputElements1=document.getElementById("input1")
+console.log(inputElement.value) 
+
+const inputElements2=document.getElementById("input2")
+console.log(inputElement.value)
+
+const inputElements3=document.getElementById("input3")
+console.log(inputElement.value)
+
+
+const inputElements1=document.getElementById("Submit")
+console.log(inputElement.value)
